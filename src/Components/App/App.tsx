b788d0185ee7ef4f65d7a22050bf58fb815d6e4d
Danila Loginov <danila@loginov.rocks>
2019-10-05 04:31:02 +0300

    First component

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+
+const App: React.FunctionComponent = () => (
+  <>
+    <h1>Hi</h1>
+
+    <p>I am Danila, nice to meet you!</p>
+
+    <h2>Social presence</h2>
+
+    <ul>
+
+      <li>
+        <a href="https://www.linkedin.com/in/loginov-rocks/">LinkedIn</a>
+      </li>
+
+      <li>
+        <a href="https://github.com/loginov-rocks">GitHub</a>
+      </li>
+
+      <li>
+        <a href="https://medium.com/@loginov_rocks">Medium</a>
+      </li>
+
+      <li>
+        <a href="https://www.drive2.com/r/toyota/chaser/288230376151952785/">DRIVE2</a>
+        {' '}
+        &mdash; my Toyota Chaser GX71 blog, originally
+        {' '}
+        <a href="https://www.drive2.ru/r/toyota/chaser/288230376151952785/">in Russian</a>
+      </li>
+
+      <li>
+        <a href="https://www.instagram.com/loginov_rocks/">Instagram</a>
+      </li>
+
+      <li>
+        <a href="https://www.facebook.com/1oginov">Facebook</a>
+      </li>
+
+      <li>
+        <a href="https://twitter.com/loginov_rocks">Twitter</a>
+      </li>
+
+      <li>
+        <a href="https://www.npmjs.com/~loginov-rocks">npm</a>
+      </li>
+
+    </ul>
+  </>
+);
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import App from 'Components/App';
+
+const container = document.getElementById('app');
+
+ReactDOM.render(<App />, container);
